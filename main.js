be1d16b966ac9180afd60e0fb02bb4b1cc951f5d
mFox <110638501+marsailifox@users.noreply.github.com>
2022-12-30 16:29:50 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("feelin lucky?")
+document.getElementById("play").addEventListener("click", function() {
+  if (document.querySelector('h2').innerText === "HIT" ||       
+  document.querySelector('h2').innerText === "STAY") {
+  document.querySelector('h2').innerText = "You'll definitely win this time!", document.querySelector('h3').innerText = ""
+} else {
+  document.querySelector('h2').innerText = "Alright, Hoss...",   
+  document.querySelector('h3').remove()}
+})
+
+document.getElementById("hitBtn").addEventListener("click", function() {
+  if (document.querySelector('h2').innerText === "Howdy Partner! Feelin' Lucky?") {
+    document.querySelector('h2').innerText = "Slow down, Hoss, press the PLAY button!"}
+  else if (document.querySelector('h2').innerText != "Slow down, Hoss, press the PLAY button!") {
+    document.querySelector('h2').innerText = "HIT"}
+})
+
+document.getElementById("stayBtn").addEventListener("click", function() {
+    if (document.querySelector('h2').innerText === "Howdy Partner! Feelin' Lucky?") {
+    document.querySelector('h2').innerText = "Slow down, Hoss, press the PLAY button!"}
+  else if (document.querySelector('h2').innerText != "Slow down, Hoss, press the PLAY button!") {
+    document.querySelector('h2').innerText = "STAY"}
+})
