14b5a7277bab570cd2b4babe0a5d4f02c2749c20
mFox <110638501+marsailifox@users.noreply.github.com>
2023-01-03 01:32:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
+const h2 = document.querySelector('h2')
+const playerTotal = document.getElementById('playerTotal')
+const dealerTotal = document.getElementById('dealerTotal')
+
+const cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10]
+
 document.getElementById("play").addEventListener("click", function() {
-  if (document.querySelector('h2').innerText === "HIT" ||       
-  document.querySelector('h2').innerText === "STAY") {
-  document.querySelector('h2').innerText = "You'll definitely win this time!"
+  if (parseInt(playerTotal.innerText) > 1) {
+  h2.innerText = "You'll definitely win this time!", playerTotal.innerText = 0, dealerTotal.innerText = 0
 } else {
-  document.querySelector('h2').innerText = "Alright, Hoss...",   
-  document.querySelector('h3').remove()}
+  h2.innerText = "Alright, boss...",   
+  document.querySelector('h3').remove(), playerTotal.innerText = cards[cards.length * Math.random() | 0],      dealerTotal.innerText = cards[cards.length * Math.random() | 0]}
 })
 
 document.getElementById("hitBtn").addEventListener("click", function() {
-  if (document.querySelector('h2').innerText === "Howdy Partner! Feelin' Lucky?") {
-    document.querySelector('h2').innerText = "Slow down, Hoss, press the PLAY button!"}
-  else if (document.querySelector('h2').innerText != "Slow down, Hoss, press the PLAY button!") {
-    document.querySelector('h2').innerText = "HIT"}
+  if (h2.innerText === "Howdy Partner! Feelin' Lucky?") {
+    h2.innerText = "Slow down, boss, press the PLAY button!"}
+  else if (h2.innerText != "Slow down, boss, press the PLAY button!" && parseInt(playerTotal.innerText) < 21 && h2.innerText != "STAY") {
+    h2.innerText = "HIT", playerTotal.innerText = parseInt(playerTotal.innerText) + cards[cards.length * Math.random() | 0]} if (playerTotal.innerText > 21) {h2.innerText = "BUSTED!"}
+  if (parseInt(playerTotal.innerText) === 21) {h2.innerText = " WOW! BLACKJACK!"}
 })
 
 document.getElementById("stayBtn").addEventListener("click", function() {
-    if (document.querySelector('h2').innerText === "Howdy Partner! Feelin' Lucky?") {
-    document.querySelector('h2').innerText = "Slow down, Hoss, press the PLAY button!"}
-  else if (document.querySelector('h2').innerText != "Slow down, Hoss, press the PLAY button!") {
-    document.querySelector('h2').innerText = "STAY"}
+  if (h2.innerText === "Howdy Partner! Feelin' Lucky?") {
+  h2.innerText = "Slow down, boss, press the PLAY button!"}
+else if (h2.innerText != "Slow down, boss, press the PLAY button!") {
+  h2.innerText = "STAY", dealerTotal.innerText = parseInt(dealerTotal.innerText) + cards[cards.length * Math.random() | 0]}
 })
