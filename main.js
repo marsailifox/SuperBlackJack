adb96172bce5c0a0e71f4a7315e5669a86b7eb2c
mFox <110638501+marsailifox@users.noreply.github.com>
2023-01-03 10:04:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const dealerTotal = document.getElementById('dealerTotal')
 
 const cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10]
 
+const dealerMove = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
 document.getElementById("play").addEventListener("click", function() {
   if (parseInt(playerTotal.innerText) > 1) {
   h2.innerText = "You'll definitely win this time!", playerTotal.innerText = 0, dealerTotal.innerText = 0
@@ -23,6 +25,15 @@ document.getElementById("hitBtn").addEventListener("click", function() {
 document.getElementById("stayBtn").addEventListener("click", function() {
   if (h2.innerText === "Howdy Partner! Feelin' Lucky?") {
   h2.innerText = "Slow down, boss, press the PLAY button!"}
-else if (h2.innerText != "Slow down, boss, press the PLAY button!") {
-  h2.innerText = "STAY", dealerTotal.innerText = parseInt(dealerTotal.innerText) + cards[cards.length * Math.random() | 0]}
+  else if (h2.innerText != "Slow down, boss, press the PLAY button!") {
+  h2.innerText = "STAY", dealerTotal.innerText = dealerMove[dealerMove.length * Math.random() | 0]}
+  if (parseInt(dealerTotal.innerText) > 21) {
+    h2.innerText = "DEALER BUSTED! YOU WIN!"
+  }
+  if (parseInt(dealerTotal.innerText) < parseInt(playerTotal.innerText)) {
+    h2.innerText = "YOU WIN!"
+  }
+  if (parseInt(dealerTotal.innerText) > parseInt(playerTotal.innerText) && parseInt(dealerTotal.innerText) < 22) {
+    h2.innerText = "HOUSE WINS! YOU LOSE!"
+  }
 })
